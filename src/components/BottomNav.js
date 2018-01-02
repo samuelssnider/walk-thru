57e95b7c3b44646b62405a944d7a5000ba1ef227
samuelssnider <sam_redins@yahoo.com>
2018-01-02 14:52:54 -0700

    add to bottom nav 2

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -3,7 +3,9 @@ import React, {Component} from "react";
 class BottomNav extends Component {
   render(){
     return(
-      <h1 className='title'>Welcome to Walk-Thru</h1>
+      <div className="bottom-nav-bar">
+      
+      </div>
     )
   }
 }
