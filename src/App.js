b4e3fb1826b2b2cea91ded7d5c673f2ffe126fdf
samuelssnider <sam_redins@yahoo.com>
2018-01-03 08:24:04 -0700

    working on routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Homepage from './components/Homepage'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import CategoriesPage from './components/CategoriesPage'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Homepage />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/categories" component = {CategoriesPage} />
+        </Switch>
+      </Router>
     );
   }
 }
 
+// <Switch>
+//   <Route exact path='/' component={Home}/>
+//   {/* both /roster and /roster/:number begin with /roster */}
+//   <Route path='/roster' component={Roster}/>
+//   <Route path='/schedule' component={Schedule}/>
+// </Switch>
+  
 export default App;
