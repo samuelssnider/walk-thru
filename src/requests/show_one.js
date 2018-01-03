b159c69f48422901f284bf26a3800531f1a7ab78
samuelssnider <sam_redins@yahoo.com>
2018-01-03 13:44:39 -0700

    working on development of show page table

--- src/requests/show_one.js
+++ src/requests/show_one.js
@@ -1,16 +1,24 @@
 import API from '../data/api'
+import collapse from '../data/collapse.png'
+import expand from '../data/expand.png'
+import instruction from '../data/instruction.png'
 const $ = require('jquery')
 
 var buildWT = function(data) {
+  console.log(data)
   var row      = document.createElement('TR')
   var imageBox = document.createElement('TD')
-  var image    = document.createElement('IMG'); image.src = data.img_source;
+  var image    = document.createElement('IMG'); image.src = instruction;
+  var expandImage = document.createElement('IMG'); expandImage.src = expand;
   var title    = document.createElement('TD'); title.innerText = data.title;
-  var content  = document.createElement('TD'); content.innerText = data.content;
+  var content  = document.createElement('TD'); content.innerText = data.description;
+  $(expandImage).addClass('expand-image')
+  $(image).addClass('instruction-image')
   row.appendChild(imageBox)
   row.appendChild(title)
   row.appendChild(content)
   imageBox.appendChild(image)
+  content.appendChild(expandImage)
   var table = document.getElementsByClassName('show-table')
   table[0].appendChild(row)
 }
@@ -21,11 +29,17 @@ const show = function(input) {
     method: 'GET',
   })
   .then(function(data) {
-    buildWT(data)
+    data[0].steps.forEach(function(element) {
+      buildWT(element)
+    })
   })
   .catch(function() {
     console.err("Failure occured")
   })
 }
+
+$(document).ready(function(){
+  
+})
 
 export default show
