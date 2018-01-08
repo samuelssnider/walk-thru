fd9540b8810456c38f62fe8d51ed73ce3c5628c1
samuelssnider <sam_redins@yahoo.com>
2018-01-07 18:03:58 -0700

    work on create bar, grabbing data from form

--- src/components/CreateBar.js
+++ src/components/CreateBar.js
@@ -1,15 +1,25 @@
 import React, {Component} from "react";
 import plus from '../data/plus.png'
 class WelcomeSign extends Component {
+  constructor(props){
+    super();
+    this.state = { title: '',
+                   description: '',
+                   image: ''      }
+  }
+  
   render(){
     return(
       <div className= 'wt-form'>
         <form >
-          <input className='wt-title-in my-input' placeholder= "Your Walk Thru Title Here"></input> <br/>
-          <input className='wt-description-in my-input' placeholder= "Your Walk Thru Description Here"></input> <br/> 
-          <input className='wt-image-in my-input' placeholder= "Your Walk Thru Image (HTML link) Here"></input> <br/>
+          <input className='wt-title-in my-input' placeholder= "Your Walk Thru Title Here"
+                 valueLink={this.linkState('email')}></input> <br/>
+          <input className='wt-description-in my-input' placeholder= "Your Walk Thru Description Here"
+                 onChange={this.handleDescriptionChange}></input> <br/> 
+          <input className='wt-image-in my-input' placeholder= "Your Walk Thru Image (HTML link) Here"
+                 onChange={this.handleImageChange}></input> <br/>
         </form>
-        <img className='create-wt-image'src={plus}/>
+        <img alt="plus" onClick={this.addWT} className='create-wt-image'src={plus}/>
       </div>
     )
   }
