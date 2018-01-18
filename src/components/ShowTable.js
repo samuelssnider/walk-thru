394c0a31512134e8a2b2c2797bc87fd36054024d
samuelssnider <sam_redins@yahoo.com>
2018-01-18 11:45:59 -0700

    adding images for creating and deleting walk_thrus

--- src/components/ShowTable.js
+++ src/components/ShowTable.js
@@ -1,10 +1,16 @@
 import React, {Component} from "react";
 import Steps from './Steps'
+import edit from '../data/Edit.png'
+import del from '../data/delete.png'
 class ShowTable extends Component {
   
   constructor() {
     super();
     this.state = { expanded: false }
+  }
+  
+  deleteWT(event){
+    alert("Are you sure you want to delete this walkthrough")
   }
   
   onChildChanged(newState) {
@@ -18,7 +24,11 @@ class ShowTable extends Component {
           <tr className="walk-thru-title">
             <td><img src={data[0].image} alt={data[0].title} className='wt-image'></img></td>
             <td className='title-wt'> {data[0].title}</td>
-            <td className='description-wt'>{data[0].description}</td>
+            <td className='description-wt'>
+              {data[0].description}
+              <img className= 'wt-delete-image' onClick={this.deleteWT} src={del} onClick={this.deleteWT} />
+              <img className= 'wt-edit-image' src={edit} onClick={this.editWT} />
+            </td>
           </tr>
         </thead>
         <Steps data={this.props.data} callbackParent={(newState) => this.onChildChanged(newState) } />
